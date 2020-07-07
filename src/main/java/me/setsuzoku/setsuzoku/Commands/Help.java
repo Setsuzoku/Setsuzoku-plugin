558fb3ccb880a12bd1cf0f9e3dc18a0fffa77424
punchinglikes <punchinglikes2000@gmail.com>
2020-07-07 00:10:20 -0400

    skeleton help command

---
+++ src/main/java/me/setsuzoku/setsuzoku/Commands/Help.java
@@ -0,0 +1,19 @@
+package me.setsuzoku.setsuzoku.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Help implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage("This command is still being worked on.");
+        }
+
+        return true;
+    }
+}
--- src/main/java/me/setsuzoku/setsuzoku/Setsuzoku.java
+++ src/main/java/me/setsuzoku/setsuzoku/Setsuzoku.java
@@ -1,12 +1,17 @@
 package me.setsuzoku.setsuzoku;
 
+import me.setsuzoku.setsuzoku.Commands.Help;
 import me.setsuzoku.setsuzoku.Events.onMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Setsuzoku extends JavaPlugin {
     @Override
     public void onEnable() {
+        getCommand("help").setExecutor(new Help());
         getServer().getPluginManager().registerEvents(new onMessage(), this);
+
+
     }
 
 }
